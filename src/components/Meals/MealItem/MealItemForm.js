61230ceb22b58e9a3a141321657caa893b809b5b
Mayen Akpan <neroxma@yahoo.com>
2021-08-06 22:42:45 -0700

    Added form

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,21 @@
 import classes from "./MealItemForm.module.css";
 import React from "react";
+import Input from "../../UI/Input";
 
 function MealItemForm() {
   return (
     <form action="" className={classes.form}>
-      <input type="text" />
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+Add</button>
     </form>
   );
