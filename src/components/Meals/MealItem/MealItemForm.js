271056d9592de369e4180bd0b9809b5a67cc8944
Mayen Akpan <neroxma@yahoo.com>
2021-08-06 18:55:10 -0700

    Updated

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,13 @@
+import classes from "./MealItemForm.module.css";
+import React from "react";
+
+function MealItemForm() {
+  return (
+    <form action="" className={classes.form}>
+      <input type="text" />
+      <button>+Add</button>
+    </form>
+  );
+}
+
+export default MealItemForm;
