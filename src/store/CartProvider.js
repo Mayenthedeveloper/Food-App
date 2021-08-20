227df0ffeac7cae3fb30675f61faa5fcb8938d40
Mayen Akpan <neroxma@yahoo.com>
2021-08-19 21:21:43 -0700

    Added context

---
+++ src/store/CartProvider.js
@@ -0,0 +1,11 @@
+import CartContext from "./cart-context";
+
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const CartContext = {};
+
+  return <CartContext.Provider>{props.children}</CartContext.Provider>;
+};
+
+export default CartProvider;
