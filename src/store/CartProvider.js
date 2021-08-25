e7df98dfdfd6d6b96ca02b6605fe01ca8b4d926f
Mayen Akpan <neroxma@yahoo.com>
2021-08-24 22:36:36 -0400

    Updated context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,40 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totaAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totaAmount + action.item.price * action.price.amount;
+    return {
+      items: updatedItems,
+      totaAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totaAmount: 0,
+    items: cartState.items,
+    totaAmount: cartState.totaAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
