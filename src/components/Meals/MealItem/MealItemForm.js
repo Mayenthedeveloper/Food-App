a0b6c414fb276569e196a9188b817061d0fd112b
Mayen Akpan <neroxma@yahoo.com>
2021-08-29 21:53:31 -0700

    Update

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,16 @@
 import classes from "./MealItemForm.module.css";
-import React from "react";
+import React, { useRef } from "react";
 import Input from "../../UI/Input";
 
 function MealItemForm() {
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form action="" className={classes.form}>
+    <form action="" className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
         input={{
